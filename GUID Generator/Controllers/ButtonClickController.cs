470d562c1d99fb582b38300b5d2ee79113638200
Oscar Urizar <oscar.m.Urizar.jr@gmail.com>
2021-12-08 08:54:48 -0500

    Add project files.

---
+++ GUID Generator/Controllers/ButtonClickController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Runtime.InteropServices;
+
+namespace GUID_Generator.Controllers
+{
+    public class ButtonClickController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Check(string button)
+        {
+
+            Guid g = Guid.NewGuid();
+            System.Console.WriteLine(g);
+
+            return View();
+        }
+    }
+}
